2e308dfabac6f5ad94148956ed21eb6e4be92a8f
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-15 11:37:54 +0100

    added fontfaceobserver

--- opdracht1/app/src/app.js
+++ opdracht1/app/src/app.js
@@ -4,6 +4,14 @@ import './styles/styles.css'
 import app from './components/init'
 import api from './components/api'
 
+import FontFaceObserver from 'fontfaceobserver'
+
 (function() {
 	app.init()
 })()
+
+var font = new FontFaceObserver('Roboto');
+
+font.load().then(function () {
+  document.querySelector('body').classList.add('font-ready')
+});
