e4260656d1a416c7b5656f155b205e2895ba334f
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-16 14:04:42 +0100

    updates

--- opdracht1/app/src/app.js
+++ opdracht1/app/src/app.js
@@ -4,14 +4,8 @@ import './styles/styles.css'
 import app from './components/init'
 import api from './components/api'
 
-import FontFaceObserver from 'fontfaceobserver'
-
 (function() {
 	app.init()
 })()
 
-var font = new FontFaceObserver('Roboto');
-
-font.load().then(function () {
-  document.querySelector('body').classList.add('font-ready')
-});
+console.log('derp')
