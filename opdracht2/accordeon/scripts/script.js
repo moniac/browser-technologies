f6a76fd7cf301c22f5324160666ba25f605834d6
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-23 14:59:13 +0100

    added files

---
+++ opdracht2/accordeon/scripts/script.js
@@ -0,0 +1,17 @@
+// function showImage()
+// {
+// 	if(this.files && this.files[0]) {
+// 		var obj = new FileReader()
+// 		obj.onload = function(data) {
+// 			var image = document.getElementById('image')
+// 			image.src = data.target.result
+// 			console.log(image.width)
+// 			image.style.display = 'block'
+// 		}
+// 		obj.readAsDataURL(this.files[0])
+// 	}
+// }
+
+// var input = document.querySelector('input')
+
+// input.addEventListener('change', showImage)
